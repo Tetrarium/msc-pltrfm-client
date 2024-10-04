6d4c87c6c85c64a7469087b1f0f5617cd1eee171
AlexandrTurov <alexandr.turov@mail.ru>
2024-10-04 23:40:40 +0700

    init project

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <>
+      <div className="center">
+        <h1>Добро пожаловать!</h1>
+        <h3>Здесь собраны лучшие треки</h3>
+      </div>
+    </>
+  );
+};
+
+export default Index;
