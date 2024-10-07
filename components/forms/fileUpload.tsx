176f4f80b36ee64e0005b817beacec2df88f50bb
AlexandrTurov <alexandr.turov@mail.ru>
2024-10-07 13:08:20 +0700

    add fileUpload component

---
+++ components/forms/fileUpload.tsx
@@ -0,0 +1,40 @@
+import React, { Dispatch, PropsWithChildren, SetStateAction, useRef } from "react";
+
+import styled from "@emotion/styled";
+import { Box, Button } from "@mui/material";
+
+interface FileUploadProps {
+  setFile: Dispatch<SetStateAction<File | null>>;
+  accept: string;
+}
+
+const VisuallyHiddenInput = styled('input')({
+  display: 'none',
+});
+
+const FileUpload: React.FC<FileUploadProps & PropsWithChildren> = ({ setFile, accept, children }) => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (files) {
+      setFile(files[0]);
+    }
+  };
+
+  return (
+    <Box onClick={() => ref.current?.click()}>
+      <VisuallyHiddenInput
+        type="file"
+        accept={accept}
+        ref={ref}
+        onChange={handleChange}
+      />
+      <Button>
+        {children}
+      </Button>
+    </Box>
+  );
+};
+
+export default FileUpload;
--- pages/tracks/create.tsx
+++ pages/tracks/create.tsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 
+import FileUpload from "@/components/forms/fileUpload";
 import TrackDescriptionForm from "@/components/forms/trackDescriptionForm";
 import StepWrapper from "@/components/stepWrapper";
 import MainLayout from "@/layouts/mainLayout";
-import { Button, Grid2, TextField, Typography } from "@mui/material";
+import { Button, Grid2, Typography } from "@mui/material";
 
 const MIN_STEP = 0;
 const MAX_STEP = 2;
 
 const Create = () => {
   const [activeStep, setActiveStep] = useState(0);
+  const [picture, setPicture] = useState<File | null>(null);
+  const [audio, setAudio] = useState<File | null>(null);
 
   const back = () => {
     if (activeStep > MIN_STEP) {
@@ -31,10 +34,14 @@ const Create = () => {
           <TrackDescriptionForm />
         }
         {activeStep === 1 &&
-          <h1>STEP 2</h1>
+          <FileUpload setFile={setPicture} accept="image/*">
+            Загрузить изображение
+          </FileUpload>
         }
         {activeStep === 2 &&
-          <h1>STEP 3</h1>
+          <FileUpload setFile={setAudio} accept="audio/*">
+            Загрузить аудио
+          </FileUpload>
         }
       </StepWrapper>
       <Grid2 container justifyContent={'center'} mt={2} gap={2}>
